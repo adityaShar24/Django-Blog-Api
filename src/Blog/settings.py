7f9e80a73329cd0cf0e9a41d8f5135ecf0174d51
adityaShar24 <adityarajansharma1234@gmail.com>
2023-11-29 13:11:47 +0530

    refactor

--- src/Blog/settings.py
+++ src/Blog/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'BlogApp',
+    'Blog_Api_App',
     'rest_framework'
 ]
 
@@ -127,7 +127,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    'DEFAULT_PERMISSION_CLASSES': []
 }
