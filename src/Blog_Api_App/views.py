6a862d18a9ed4036f71d39aab6cf163b3bf5cec1
adityaShar24 <adityarajansharma1234@gmail.com>
2023-11-29 14:16:37 +0530

    Added post blog view

--- src/Blog_Api_App/views.py
+++ src/Blog_Api_App/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import BlogSerializer
 
-# Create your views here.
+
+@api_view(['POST'])
+def post_blog(request):
+    serializer = BlogSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
