b497bc85b52e8018f06fd8f34ab4c7542a8e7737
adityaShar24 <adityarajansharma1234@gmail.com>
2023-11-29 14:46:48 +0530

    Added get all blogs url

--- src/Blog_Api_App/urls.py
+++ src/Blog_Api_App/urls.py
@@ -4,5 +4,5 @@ from .views.blog_view import post_blog , get_all_blogs
 
 urlpatterns = [
     path('blog/post/', post_blog , name='post_blog'),
-    path('blog/get/', get_all_blogs , name='get_all_blogs')
+    path('blog/get-all/', get_all_blogs , name='get_all_blogs')
 ]
