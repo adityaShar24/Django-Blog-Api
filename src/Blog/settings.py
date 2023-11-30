3ae30742f1c89014125233359b5180b11314bbf4
adityaShar24 <adityarajansharma1234@gmail.com>
2023-11-30 12:44:28 +0530

    Added default authentication

--- src/Blog/settings.py
+++ src/Blog/settings.py
@@ -127,5 +127,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': []
+    'DEFAULT_PERMISSION_CLASSES': [],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
 }
