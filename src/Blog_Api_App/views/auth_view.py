fbd70c31b8b4188e4cd08602aec61116ca63129d
adityaShar24 <adityarajansharma1234@gmail.com>
2023-11-30 19:39:58 +0530

    Refactor

--- src/Blog_Api_App/views/auth_view.py
+++ src/Blog_Api_App/views/auth_view.py
@@ -1,10 +1,9 @@
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from ..serializers import UserSerializer
+from ..serializers import UserSerializer , LoginSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 
@@ -18,22 +17,26 @@ def register_user(request):
 
     
 @api_view(['POST'])
-# @authentication_classes([TokenAuthentication])
-@permission_classes([AllowAny])
 def login_user(request):
-    username = request.data.get('username')
-    password = request.data.get('password')
+    data = request.data
+    
+    serializer = LoginSerializer(data=data)
+    
+    if serializer.is_valid(raise_exception=True):
+        username = serializer.data['username']
+        password = serializer.data['password']
+        
+        user = authenticate(username=username, password=password)
+        
+        if user is None:
+            return Response({'error': 'Invalid credentials'}, status=400)
+        
+        refresh = RefreshToken.for_user(user)
 
-    user = authenticate(username=username, password=password)
-
-    if user:
-        token, created = Token.objects.get_or_create(user=user)
-        return Response({'token': token.key}, status=200)
-    else:
-        return Response({'error': 'Invalid credentials'}, status=400)
-
-# {
-# "username" : "Ashish" , 
-# "email" : "ashish@email.com",
-# "password" : "ash123"
-# }
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }
+        
+        
+    return Response(serializer.errors, status=400)
