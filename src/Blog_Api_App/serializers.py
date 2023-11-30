6aa928d0a2d6e4cd6fda53e128b3da2fae2193af
adityaShar24 <adityarajansharma1234@gmail.com>
2023-11-30 12:37:11 +0530

    Added user serializer

--- src/Blog_Api_App/serializers.py
+++ src/Blog_Api_App/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models.blog_model import Blog
 
 class BlogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Blog
         fields = '__all__'
-        
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['_id', 'username', 'email', 'password']
