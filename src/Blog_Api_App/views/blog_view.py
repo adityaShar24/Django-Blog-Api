1bdbafd4ec57bf187d57ed5d44d445b3331ea105
adityaShar24 <adityarajansharma1234@gmail.com>
2023-12-04 16:47:19 +0530

    Added authentication classes

--- src/Blog_Api_App/views/blog_view.py
+++ src/Blog_Api_App/views/blog_view.py
@@ -1,14 +1,17 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 from ..serializers import BlogSerializer
 from ..models import Blog
 
-
 @api_view(['POST'])
+@authentication_classes([SessionAuthentication])
+@permission_classes([IsAuthenticated])
 def post_blog(request):
     serializer = BlogSerializer(data=request.data)
     if serializer.is_valid(raise_exception=True):
-        serializer.save()
+        serializer.save(author=request.user)
         return Response(serializer.data, status=201)
     return Response(serializer.errors, status=400)
 
