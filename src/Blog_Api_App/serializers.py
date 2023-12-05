3a775c55ce2d46493a525077b28a12c712d457d6
adityaShar24 <adityarajansharma1234@gmail.com>
2023-12-05 17:07:34 +0530

    refactor

--- src/Blog_Api_App/serializers.py
+++ src/Blog_Api_App/serializers.py
@@ -3,16 +3,10 @@ from django.contrib.auth.models import User
 from .models.blog_model import Blog
 
 class BlogSerializer(serializers.ModelSerializer):
-    author = serializers.HiddenField(default=serializers.CurrentUserDefault())
-
     class Meta:
         model = Blog
         fields = ['title', 'content', 'author', 'date']
-
-    def create(self, validated_data):
-        user = self.context['request'].user if 'request' in self.context else None
-        validated_data['author'] = user
-        return super().create(validated_data)    
+        
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     class Meta:
