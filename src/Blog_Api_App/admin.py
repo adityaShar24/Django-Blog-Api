39e8e58576443f89789b95512383d8fb39c30c1e
adityaShar24 <adityarajansharma1234@gmail.com>
2023-12-06 14:22:33 +0530

    Added cutom user admin

--- src/Blog_Api_App/admin.py
+++ src/Blog_Api_App/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models.blog_model import Blog
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
+
+# your_app/admin.py
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('id', 'username', 'email', 'user_id')
+
+    def user_id(self, obj):
+        return obj.id
+    user_id.short_description = 'User ID'
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Blog)
 
-# Register your models here.
