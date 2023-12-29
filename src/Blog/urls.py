606d7807bd37256963d26d862d8c276ebd99526c
adityaShar24 <adityarajansharma1234@gmail.com>
2023-12-29 15:19:15 +0530

    Added auth urls

--- src/Blog/urls.py
+++ src/Blog/urls.py
@@ -19,5 +19,5 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('Blog_Api_App.urls')),
+    path('api/auth/',include('Blog_Api_App.urls.auth_urls')),
 ]
