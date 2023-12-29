f336b7fb53ac2584cf4a5703279d9b00c60fe5df
adityaShar24 <adityarajansharma1234@gmail.com>
2023-12-29 14:49:19 +0530

    Refactor

--- src/Blog_Api_App/views/auth_view.py
+++ src/Blog_Api_App/views/auth_view.py
@@ -2,42 +2,58 @@ from django.contrib.auth import authenticate
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from ..serializers import UserSerializer , LoginSerializer
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.tokens import RefreshToken , AccessToken
+from ..utils.constants import USER_REGISTERED_MESSAGE , USER_LOGGEDIN_MESSAGE , INVAID_CREDENTIALS_MESSAGE
+from rest_framework.status import HTTP_201_CREATED , HTTP_400_BAD_REQUEST
 
 
 
 @api_view(['POST'])
 def register_user(request):
+    response = None
+    
     serializer = UserSerializer(data=request.data)
+    
     if serializer.is_valid(raise_exception=True):
         user = serializer.save()
         user.set_password(serializer.validated_data['password'])
         user.save()
-        return Response(serializer.data, status=201)
-    return Response(serializer.errors, status=400)
+        
+        respone_data = {
+            "message": USER_REGISTERED_MESSAGE,
+            "data": serializer.data
+        }
+        
+        
+        response = Response( respone_data , status= HTTP_201_CREATED)
+    else:
+        response = Response(serializer.errors , status= HTTP_400_BAD_REQUEST) 
+    
+    return response
 
     
 @api_view(['POST'])
 def login_user(request):
-    data = request.data
+    response = None
     
-    serializer = LoginSerializer(data=data)
+    serializer = LoginSerializer(data = request.data)
     
-    if serializer.is_valid(raise_exception=True):
-        username = serializer.data['username']
+    if serializer.is_valid():
         password = serializer.data['password']
+        username = serializer.data['username']
         
-        user = authenticate(username=username, password=password)
+        user = authenticate( request, username= username , password = password )
         
         if user is None:
-            return Response({'error': 'Invalid credentials'}, status=400)
-        
+            response = Response({"message": INVAID_CREDENTIALS_MESSAGE } , status= HTTP_400_BAD_REQUEST)
+    
         refresh = RefreshToken.for_user(user)
+        access = AccessToken.for_user(user)
 
-        return Response({
-            'refresh': str(refresh),
-            'access': str(refresh.access_token),
-        })
-        
-        
-    return Response(serializer.errors, status=400)
+        response =  Response(
+            {
+                'message': USER_LOGGEDIN_MESSAGE.format(username = username) ,
+                'refresh': str(refresh),
+                'access': str(access)
+            }
+        )
