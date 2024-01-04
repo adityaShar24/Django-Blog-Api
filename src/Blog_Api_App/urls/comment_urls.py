9516d132115277a0715ed33189550a8d1be46cd1
adityaShar24 <adityarajansharma1234@gmail.com>
2024-01-04 16:54:06 +0530

    Added urls

--- src/Blog_Api_App/urls/comment_urls.py
+++ src/Blog_Api_App/urls/comment_urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from ..views.comment_view import create_comment
+from ..views.comment_view import create_comment , update_comment
 
 
 urlpatterns = [
-    path('comment' , create_comment , name='comment'),
+    path('create' , create_comment , name='comment'),
+    path('update' , update_comment , name='update'),
 ]
 
